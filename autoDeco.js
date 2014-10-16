3f620e34b588fd504ac1a11b2f3dae4a696a0930
Nightmaster <nightmaster@seitaer.com>
2014-10-17 00:45:43 +0200

    Fix stop process before all action performed Signed-off-by: Nightmaster <nightmaster@seitaer.com>

--- autoDeco.js
+++ autoDeco.js
@@ -14,7 +14,6 @@ getAllCookies = function()
 		{
 			cbReset(rows);
 			cbErase(rows);
-			process.exit();
 		}
 		else
 		{
@@ -28,9 +27,9 @@ cbReset = function (res)
 {
 	for(var i = 0; i < res.length; i++)
 	{
-		var validity = JSON.stringify(res[i].validity).split(/[-\. :T]/);
+		var validity = JSON.stringify(res[i].substring(1).validity).split(/[-\. :T]/);
 		if(new Date() >= new Date(validity[0], validity[1]-1, validity[2], validity[3], validity[4], validity[5]))
-			connection.query('Update user \nSet user_ip = "", user_connected = 0, user_port = "-1" \n Where id = ' + res[i].id + ';', function(err, rows)
+			connection.query('Update user \nSet user_ip = "", user_connected = 0, user_port = -1 \n Where id = ' + res[i].id + ';', function(err, rows)
 			{
 				if (err)
 					console.error(err);
@@ -42,12 +41,14 @@ cbErase = function(res)
 {
 	for(var i = 0; i < res.length; i++)
 	{
-		var validity = JSON.stringify(res[i].validity).split(/[-\. :T]/);
+		var validity = JSON.stringify(res[i].validity).substring(1).split(/[-\. :T]/);
 		if(new Date() >= new Date(validity[0], validity[1]-1, validity[2], validity[3], validity[4], validity[5]))
-			connection.query('Delete From cookie \n Where id = ' + res[i].id + ';', function(err, rows)
+			connection.query('Delete From cookie \n Where user_id = ' + res[i].id + ';', function(err, rows)
 			{
 				if (err)
 					console.error(err);
+				if(i === res.length)
+					process.exit();
 			});
 	}
 
